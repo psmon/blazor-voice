1dcd950dc1ab391c51478271590a26f3b1bd8eb3
psmon <psmon@live.co.kr>
2025-06-06 16:14:50 +0900

    ActorSystem 초기 셋팅

--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
+ï»¿using BlazorVoice.Akka;
 using BlazorVoice.Components;
 using BlazorVoice.Services;
 
 using MudBlazor.Services;
 
+using NLog;
+using NLog.Config;
+using NLog.Web;
+
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+
 var builder = WebApplication.CreateBuilder(args);
+
+// NLog ì„¤ì • íŒŒì¼ ë¡œë“œ
+var env = builder.Environment.EnvironmentName;
+var nlogConfigFile = env switch
+{
+    "Development" => "NLog.Development.config",
+    "Local" => "NLog.Local.config",
+    _ => "NLog.config"
+};
+builder.Logging.ClearProviders();
+LogManager.Configuration = new XmlLoggingConfiguration(nlogConfigFile);
+builder.Host.UseNLog();
+
+logger.Info("Start BlazorVoice Service API");
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -16,10 +39,24 @@ builder.Services.AddSignalR(options =>
     options.MaximumReceiveMessageSize = 1024 * 1024; // 1MB
 });
 
-// OpenAIService¸¦ DI ÄÁÅ×ÀÌ³Ê¿¡ µî·Ï
+
+//DI Singleton
+builder.Services.AddSingleton<AkkaService>();
+
+
+//DI Scoped
 builder.Services.AddScoped<OpenAIService>();
 
+//DI Transient for DB
+
 var app = builder.Build();
+
+
+//AKKA System
+var akkaService = app.Services.GetRequiredService<AkkaService>();
+
+var actorSystem = akkaService.CreateActorSystem("default");
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
